e89848702853e5a589967b522487a4a0a107e276
Taniya Mary Varghese <taniyavarghese@Taniyas-MacBook-Air.local>
2024-09-16 13:05:24 -0700

    Upload add two int

---
+++ Lab0/Addint/src/AddTwoInt.java
@@ -0,0 +1,9 @@
+public class AddTwoInt {
+  public static void main(String[] args) {
+    int num1 = 2;
+    int num2 = 3;
+    int result = num1 + num2;
+    System.out.println(num1 + " + " + num2 + " = " + result);
+  }
+
+}
